c2191d682990d3831199eae2a987158618fd3f3a
lucasmavila <lucas.mavila94@gmail.com>
2021-03-03 19:28:35 -0300

    ExercicioFinal Date

--- src/main/java/aulas/trabalhandoComDatas/javaDate/ExercicioFinal.java
+++ src/main/java/aulas/trabalhandoComDatas/javaDate/ExercicioFinal.java
@@ -1,4 +1,24 @@
 package aulas.trabalhandoComDatas.javaDate;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class ExercicioFinal {
+    public static void main(String[] args) throws Exception{
+
+        String nascimentoStr = "15/06/1994";
+        String dataRefStr = "15/05/2010";
+        SimpleDateFormat formatoSdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Date nascimentoFormatada = formatoSdf.parse(nascimentoStr);
+        long milis = nascimentoFormatada.getTime();
+
+        Date dataRef = formatoSdf.parse(dataRefStr);
+
+        int comparacao = nascimentoFormatada.compareTo(dataRef);
+        System.out.println(milis);
+        System.out.println(nascimentoFormatada);
+        System.out.println(comparacao);
+
+    }
 }
